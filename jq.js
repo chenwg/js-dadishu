b97c417b1834abcf8de8ab96ea7bfdc9998118bc
chenchen <chenchen>
2016-11-14 11:15:50 +0800

    2016-11-14

--- jq.js
+++ jq.js
@@ -1,47 +1,61 @@
 	var at = document.getElementById('at'),
 		ht=document.getElementById('ht'),
+		step=document.getElementById('step'),
+		hs=document.getElementById('hs'),
 		st=document.getElementById('st'),
 		r = document.getElementById('r'),
-		w = document.getElementById('w');
+		w = document.getElementById('w'),
+		l = document.getElementById('l');
 	var pl;
-	var alltime = at.value*60;
+	var alltime = 1;
+	var steps = 1;
 	var spList = document.getElementsByTagName('span');
 	var spLen = spList.length;
-	var bef=999999;
+	var bef=0;
 	var adds = true;
 	var clickOne = true;
 	var rnum = 0;
 	var wnum = 0;
-	function start(){
-		pl = window.setInterval('showColor()',1000)
+	var lnum = 0;
+	function start(type){
+		window.clearInterval(pl);
+		switch (type)
+		{
+			case 1:
+				alltime = at.value*60;
+				break;
+			case 2:
+				steps = step.value;
+				break;
+			default:
+				break;
+		}
+		for(var i=0;i<spLen;i++){
+			spList[i].addEventListener('click',function(e){
+				for(var j=0;j<spLen;j++){
+					if(spList[j] == this){
+						if(clickOne){
+							if(j == bef){
+								r.innerText = ++rnum;
+							}else{
+								w.innerText = ++wnum;
+							}
+							clickOne = false;
+						}
+					}
+							
+				}
+			})
+		}
+		pl = window.setInterval('showColor('+type+')',1000)
 	}
-	
-	function showColor(){
-		if(alltime == 0){
+
+	var showColor = function(type){
+		if(alltime == 0 || steps == 0){
 			window.clearInterval(pl);
 			return false;
 		}
-		for(var i=0;i<spLen;i++){
-			spList[i].style.backgroundColor = '#FFF';
-			if(adds){
-				spList[i].addEventListener('click',function(e){
-					for(var j=0;j<spLen;j++){
-						if(spList[j] == this){
-							if(clickOne){
-								if(j == bef){
-									r.innerText = ++rnum;
-								}else{
-									w.innerText = ++wnum;
-								}
-								clickOne = false;
-							}
-						}
-							
-					}
-				})
-			}
-		}
-		adds = false;
+		spList[bef].style.backgroundColor = '#FFF';
 		var mt = Math.round(Math.random()*8);
 		if(mt == 0 && mt == bef){
 			mt = 1; 
@@ -50,7 +64,21 @@
 		}
 		spList[mt].style.backgroundColor = '#C9AD20';
 		bef = mt;
-		alltime -= 1;
-		ht.innerText = alltime;
+		switch (type)
+		{
+			case 1:
+				alltime -= 1;
+				ht.innerText = alltime;
+				break;
+			case 2:
+				steps -= 1;
+				hs.innerText = steps;
+				break;
+			default:
+				break;
+		}
+		if(clickOne){
+			l.innerText = ++lnum;
+		}
 		clickOne = true;
 	}
